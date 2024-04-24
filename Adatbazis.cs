51f3f6f6a226d3f22d43c62b276766ad322d5c10
Pistipisti94 <pistipisti57@gmail.com>
2024-04-24 09:40:26 +0200

    Add project files.

---
+++ Adatbazis.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+
+namespace DolgozokConsole
+{
+    internal class Adatbazis
+    {
+        MySqlCommand command;
+        MySqlConnection connection; 
+        public Adatbazis() 
+        {
+            MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder();
+            builder.Server = "localhost";
+            builder.UserID = "root";
+            builder.Database = "dolgozok";
+            builder.Password = "";
+
+            connection = new MySqlConnection(builder.ConnectionString);
+            command = connection.CreateCommand();
+            try
+            {
+                KapcsolatNyit();
+                KapcsolatZar();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+
+        }
+
+        private void KapcsolatZar()
+        {
+            if (connection.State != System.Data.ConnectionState.Closed)
+            {
+                connection.Close();
+            }
+        }
+
+        private void KapcsolatNyit()
+        {
+            if (connection.State != System.Data.ConnectionState.Open)
+            {
+                connection.Open();
+            }
+        }
+
+        internal List<Dolgozok> getAllDolgozo()
+        {
+            List<Dolgozok> dolgozoks = new List<Dolgozok>();
+
+            return dolgozoks;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DolgozokConsole
+{
+    internal class Program
+    {
+        static Adatbazis db = new Adatbazis();
+        static List<Dolgozok> dolgozoks;
+        static void Main(string[] args)
+        {
+            dolgozoks = db.getAllDolgozo();
+            Console.WriteLine("Program vége!");
+            Console.ReadLine();
+        }
+    }
+}
